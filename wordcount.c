22feac19c35db0374d9ec2915b5b84723eaf8e24
Kevein Liu <liuyang3240@gmail.com>
2011-08-17 08:45:01 +0800

    Early version

---
+++ wordcount.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printarray(int **a);
+
+int main()
+{
+	char s[1024];	
+	int i;
+	while(0){
+	
+	}
+	exit(0);
+}
+
+void printarray(int a[][])
+{
+
+}
